67de83323ea39b8dd7daa582867a5e632a365fca
Nikhil98765 <42944335+Nikhil98765@users.noreply.github.com>
2024-11-07 17:25:08 +0530

    episode - 1 changes

---
+++ App.js
@@ -0,0 +1,35 @@
+/**
+ * <div id="parent">
+ *  <div id="child1">
+ *    <h1>I am an h1 tag</h1>
+ *    <h2>I am an h2 tag</h2>
+ *  </div>
+ * </div>
+ */
+
+const parent = React.createElement('div', {id: 'parent'},
+  [
+    React.createElement('div', { id: 'child1'}, [
+      React.createElement('h1', {}, 'I am an h1 tag'),
+      React.createElement('h2', {}, "I'm an h2 tag")
+    ]),
+    React.createElement('div', { id: 'child2'}, [
+      React.createElement('h1', {}, 'I am an h1 tag'),
+      React.createElement('h2', {}, "I'm an h2 tag")
+    ])
+  ]
+)
+console.log("🚀 ~ parent:", parent)
+
+
+// Creates an object of type reactElement
+const heading = React.createElement('h1', {
+  id: 'heading',
+  prop: 'abc'
+}, "Hello world from React!");
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Converts the passed reactElement into browser node and append it to root
+root.render(parent);

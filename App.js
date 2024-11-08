cf1ed5792e46ed8ded675f01257183e3c0f6bdfc
Nikhil98765 <42944335+Nikhil98765@users.noreply.github.com>
2024-11-08 19:09:52 +0530

    Episode - 3 changes

--- App.js
+++ App.js
@@ -1,38 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-/**
- * <div id="parent">
- *  <div id="child1">
- *    <h1>I am an h1 tag</h1>
- *    <h2>I am an h2 tag</h2>
- *  </div>
- * </div>
- */
-
-const parent = React.createElement('div', {id: 'parent'},
-  [
-    React.createElement('div', { id: 'child1'}, [
-      React.createElement('h1', {}, 'I am an h123 tag'),
-      React.createElement('h2', {}, "I'm an h2 tag")
-    ]),
-    React.createElement('div', { id: 'child2'}, [
-      React.createElement('h1', {}, 'I am an h1 tag'),
-      React.createElement('h2', {}, "I'm an h2 tag")
-    ])
-  ]
-)
-console.log("🚀 ~ parent:", parent)
-
+// Functional component
+const Title = () => {
+  return <h1 className="title" tabIndex="5">
+    This is title component
+  </h1>;
+}
 
 // Creates an object of type reactElement
-const heading = React.createElement('h1', {
-  id: 'heading',
-  prop: 'abc'
-}, "Hello world from React!");
+// const title = (
+//   <h1 className="title" tabIndex="5">
+//     This is title component
+//   </h1>
+// );
 
+// * Functional component is a function which returns React element / JSX element
+function HeadingComponent() {
+  return (
+    // component composition
+    <div id="container">
+      {/* rendering functional component */}
+      {/* <Title /> */}
+      {/*{Title()}*/}
+      <Title></Title>
+
+      {/* rendering react element */}
+      {/*{title}*/}
+
+      <h1 className="heading" tabIndex="3">
+        This is a heading component
+      </h1>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // Converts the passed reactElement into browser node and append it to root
-root.render(parent);
+root.render(<HeadingComponent />);

fde402bd924aa55923e69175a490bd0042d8c7e8
Nikhil98765 <42944335+Nikhil98765@users.noreply.github.com>
2025-01-14 13:29:54 +0530

    Added login & logout and search functionality and fixed the search bugs using another state variable

--- src/components/Body.js
+++ src/components/Body.js
@@ -7,6 +7,9 @@ import {Shimmer} from "./Shimmer";
 export const Body = () => {
 
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredListOfRestaurants, setFilteredListOfRestaurants] = useState([]);
+
+  const [searchText, setSearchText] = useState('');
 
   useEffect(() => {
     fetchData();
@@ -15,7 +18,9 @@ export const Body = () => {
   async function fetchData() {
     const response = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.956708347221225&lng=77.6610016822815&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
     const data = await response.json();
-    setListOfRestaurants(data.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+    const resList = data.data.cards[1].card.card.gridElements.infoWithStyle.restaurants;
+    setListOfRestaurants(resList);
+    setFilteredListOfRestaurants(resList);
   }
 
 
@@ -29,17 +34,32 @@ export const Body = () => {
   return (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input type="text" value={searchText}
+                 onChange={(e) =>
+                   setSearchText(e.target.value)
+          } />
+
+          <button onClick={() => {
+            const filteredRestaurants = listOfRestaurants.filter(restaurant => {
+              return restaurant.info.name.toLowerCase().includes(searchText.toLowerCase());
+            })
+            setFilteredListOfRestaurants(filteredRestaurants);
+          }}>Search</button>
+
+        </div>
+
         <button className="filter-btn"
         onClick={() => {
           const filteredList = listOfRestaurants.filter(item => item.info.avgRating > 4);
-          setListOfRestaurants(filteredList);
+          setFilteredListOfRestaurants(filteredList);
         }}>
           Top Rated Restaurants
         </button>
       </div>
       <div className="res-container">
         {
-          listOfRestaurants.map(res => <RestaurantCard key={ res.info.id } resData={res}/>)
+          filteredListOfRestaurants.map(res => <RestaurantCard key={ res.info.id } resData={res}/>)
         }
       </div>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
 import {LOGO_URL} from "../utils/constants";
+import {useState} from "react";
 
 export const Header = () => {
+
+  let [btnName, setBtnName] = useState('Login');
+
   return (
     <div className="header">
       <div className="logo-container">
@@ -14,6 +18,11 @@ export const Header = () => {
           <li>About</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button className="login-btn"
+            onClick={() => {
+              btnName === 'Login' ? setBtnName('Logout'): setBtnName('Login');
+            }}
+          >{btnName}</button>
         </ul>
       </div>
     </div>
